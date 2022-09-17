e690eee49c1331bdb2750fc04d872e4cc292f57b
Evgenii Milshin <zhenyamilshin@gmail.com>
2022-09-18 02:35:40 +0300

    autotest

--- src/test/java/com/demoqa/StudentRegistrationForm.java
+++ src/test/java/com/demoqa/StudentRegistrationForm.java
@@ -22,17 +22,23 @@ import static com.codeborne.selenide.Selenide.open;
     void fillFormTest() {
 
         open("automation-practice-form");
+
         $("#firstName").setValue("Evgenii");
         $("#lastName").setValue("Milshin");
+
         $("#userEmail").setValue("zhenyamilshin@gmail.com");
         $("#genterWrapper").$(byText("Male")).click();
+
         $("#userNumber").setValue("9107479861");
         $("[id=dateOfBirthInput]").click();
+
         $(".react-datepicker__year-select").selectOption("2000");
         $(".react-datepicker__month-select").selectOption("March");
         $(".react-datepicker__day--016").click();
+
         $("#subjectsInput").setValue("Maths").pressEnter();
         $("#hobbiesWrapper").$(byText("Reading")).click();
+
         $("#uploadPicture").uploadFromClasspath("23.jpg");
         $("#currentAddress").setValue("г.Курск, ул.Сторожевая 6А");
 
